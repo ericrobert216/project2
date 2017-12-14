5c21ff6bb47c9656f5a0f79f3857f61ecec6e894
ericrobert216 <ericrobert216@gmail.com>
2017-12-14 10:51:55 +0700

    aaaa

--- frontend/hook.php
+++ frontend/hook.php
@@ -14,5 +14,60 @@ add_action( 'woocommerce_before_shop_loop', 'Zoo\Frontend\Hook\add_zoo_ln_naviga
 
 
 function add_zoo_ln_navigation(){
+
+    global $wp_query;
+
+    var_dump($_POST);
+
+    //echo('<pre/>');
+
+    //var_dump($wp_query->query);
+
+    $arg = $wp_query->query;
+
+    $arg['meta_query'] = array(
+        'relation' => 'AND',
+        array(
+           'key'     => '_regular_price',
+           'value'   => array( 0, 6000 ),
+           'type'    => 'numeric',
+           'compare' => 'BETWEEN',
+        ),
+        array(
+            'key'     => '_stock_status',
+            'value'   => 'instock',
+            'compare' => '='
+        ),
+        array(
+            'key'     => '_wc_average_rating',
+            'value'   => 0,
+            'type'    => 'numeric',
+            'compare' => '>='
+        ),
+        array(
+            'key'     => '_wc_review_count',
+            'value'   => 0,
+            'type'    => 'numeric',
+            'compare' => '>='
+        )
+    );
+
+
+    $arg['tax_query'] = array(
+        array(
+            'taxonomy' => 'product_cat',
+            'field'    => 'term_id',
+            'terms'    => array( 17,16 ),
+        )
+    );
+
+
+    $my_query = new \WP_Query( $arg );
+
+    $wp_query = null;
+    $wp_query = $my_query;
+
+    //var_dump($wp_query->request);
+
     require_once ZOO_LN_TEMPLATES_PATH.'view.php';
 }
--- templates/admin/filter-setting/attribute.php
+++ templates/admin/filter-setting/attribute.php
@@ -10,24 +10,30 @@
 <?php
     $item_type = 'attribute';
     $content_title = '';
-
-//    var_dump($filter_data);
-//    $filter_item_type = $filter_data['filter_item_type'];
-
+    $attribute_ids = '';
+    $add_new = 'multi';
 
     if (isset($item_data)) {
         $item_number = $item_data['item_number'];
-        $multi_number = $array_multi_number[$item_type];
 
         $sac = (array)json_decode($item_data['item_config_value']['filter_config_value']);
+        $sac = (array)reset($sac);
+
+        var_dump($sac);
 
         $content_title = $sac['title'];
+        $attribute_ids = $sac['attribute-ids'];
+        $add_new = '';
 
-//        echo('<pre/>');
-//        var_dump($sac);
-//        die;
+        var_dump($attribute_ids);
+
     } else {
         $item_number = '__i__';
+    }
+
+    if (isset($array_multi_number[$item_type])) {
+        $multi_number = $array_multi_number[$item_type];
+    } else {
         $multi_number = 1;
     }
 ?>
@@ -48,23 +54,23 @@
             <div class="item-content">
                 <p>
                     <label for="attribute-<?php echo $item_number?>-title">Title</label>
-                    <input class="widefat " id="attribute-<?php echo $item_number?>-title" name="attribute[<?php echo $item_number?>][title]" value="Active filters" type="text">
+                    <input class="widefat " id="attribute-<?php echo $item_number?>-title" name="attribute[<?php echo $item_number?>][title]" value="<?php echo($content_title);?>" type="text">
                 </p>
                 <p>
                     <label for="attribute-<?php echo $item_number?>-attribute-ids">Display type</label>
                     <select class="widefat " id="attribute-<?php echo $item_number?>-attribute-ids" name="attribute[<?php echo $item_number?>][attribute-ids]">
-                        <option value="attribute-1">Attribute 1</option>
-                        <option value="attribute-2">Attribute 2</option>
-                        <option value="attribute-3">Attribute 3</option>
-                        <option value="attribute-4">Attribute 4</option>
-                        <option value="attribute-5">Attribute 5</option>
-                        <option value="attribute-6">Attribute 6</option>
-                        <option value="attribute-7">Attribute 7</option>
-                        <option value="attribute-8">Attribute 8</option>
-                        <option value="attribute-9">Attribute 9</option>
-                        <option value="attribute-10">Attribute 10</option>
-                        <option value="attribute-11">Attribute 11</option>
-                        <option value="attribute-12">Attribute 12</option>
+                        <option value="attribute-1" <?php selected($attribute_ids,'attribute-1');?>>Attribute 1</option>
+                        <option value="attribute-2" <?php selected($attribute_ids,'attribute-2');?>>Attribute 2</option>
+                        <option value="attribute-3" <?php selected($attribute_ids,'attribute-3');?>>Attribute 3</option>
+                        <option value="attribute-4" <?php selected($attribute_ids,'attribute-4');?>>Attribute 4</option>
+                        <option value="attribute-5" <?php selected($attribute_ids,'attribute-5');?>>Attribute 5</option>
+                        <option value="attribute-6" <?php selected($attribute_ids,'attribute-6');?>>Attribute 6</option>
+                        <option value="attribute-7" <?php selected($attribute_ids,'attribute-7');?>>Attribute 7</option>
+                        <option value="attribute-8" <?php selected($attribute_ids,'attribute-8');?>>Attribute 8</option>
+                        <option value="attribute-9" <?php selected($attribute_ids,'attribute-9');?>>Attribute 9</option>
+                        <option value="attribute-10" <?php selected($attribute_ids,'attribute-10');?>>Attribute 10</option>
+                        <option value="attribute-11" <?php selected($attribute_ids,'attribute-11');?>>Attribute 11</option>
+                        <option value="attribute-12" <?php selected($attribute_ids,'attribute-12');?>>Attribute 12</option>
                     </select>
                 </p>
                 <p>
@@ -92,7 +98,7 @@
             <input name="item-height" class="item-height" value="200" type="hidden">
             <input name="item_number" class="item_number" value="-1" type="hidden">
             <input name="multi_number" class="multi_number" value="<?php echo $multi_number;?>" type="hidden">
-            <input name="add_new" class="add_new" value="multi" type="hidden">
+            <input name="add_new" class="add_new" value="<?php echo($add_new);?>" type="hidden">
             <div class="item-control-actions">
                 <div class="alignleft">
                     <button type="button" class="button-link button-link-delete item-control-remove">Delete</button> |
